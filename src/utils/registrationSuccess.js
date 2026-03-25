6d9ca1d104d768dd8e63b59b01109f46fe45df1b
kausik <kkausik11@g,ail.com>
2026-03-25 10:43:23 +0530

    add registration success mail template

--- src/utils/registrationSuccess.js
+++ src/utils/registrationSuccess.js
@@ -1,8 +1,32 @@
 module.exports = (name) => `
-  <h2>Welcome to Save Medha Foundation</h2>
+  <h2>Welcome to Medha Botanics 🌿</h2>
+
   <p>Dear ${name.firstName} ${name.lastName},</p>
-  <p>Your registration has been successfully completed.</p>
-  <p>You can now log in and start using the system.</p>
+
+  <p>
+    We’re delighted to welcome you to <strong>Medha Botanics</strong>.
+    Your registration has been successfully completed.
+  </p>
+
+  <p>
+    You can now access your account and explore our range of natural wellness solutions designed to support your health from within.
+  </p>
+
+  <p>
+    👉 <a href="https://medha-bota.vercel.app/" target="_blank">
+    Click here to visit your dashboard
+    </a>
+  </p>
+
   <br />
-  <p>Thank you,<br/>Save Medha Foundation</p>
+
+  <p>
+    If you have any questions or need assistance, feel free to reach out to us anytime.
+  </p>
+
+  <p>
+    Thank you for choosing us.<br/>
+    <strong>Team Medha Botanics</strong><br/>
+    Powered by Save Medha Foundation
+  </p>
 `;
